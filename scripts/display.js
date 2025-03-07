4233f05876635c226f1ace4ead25b9953f4d3d2b
FES001 <firdavs7794948@gmail.com>
2025-03-08 02:00:22 +0500

    Working on it.

--- scripts/display.js
+++ scripts/display.js
@@ -1,1 +1,22 @@
 const outputContainer = document.querySelector('.main-output');
+
+// RETRIEVING THE DATA
+
+async function retrievingTheData() {
+    try {
+        const response = await fetch('../challenges-data/challenges-data.json');
+
+        if (!response.ok) {
+            throw new Error(response.status);
+        };
+
+        const data = await response.json();
+
+        
+    }
+    catch(error) {
+        console.error(error);
+    };
+};
+
+retrievingTheData();
