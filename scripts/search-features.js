80dac67417a83a320105fc27ecf74f56330bf7ca
FES001 <firdavs7794948@gmail.com>
2025-03-07 21:40:01 +0500

    Working on it.

--- scripts/search-features.js
+++ scripts/search-features.js
@@ -1,18 +1,18 @@
 // SEARCH
 const searchInput = document.querySelector('#searchInput');
-const searchButton = document.querySelector('#searchButton');
+const searchCharacterRemoveButton = document.querySelector('#searchCharacterRemoveButton');
 
 // SHOWING THE REMOVE THE CHARACTERS BUTTON AS SOON AS AN INPUT IS ENTERED
 searchInput.addEventListener('input', e => {
     if (e.target.value.length > 0) {
-        searchButton.classList.add('main-search-input-button-shown');
+        searchCharacterRemoveButton.classList.add('main-search-input-button-shown');
     } else {
-        searchButton.classList.remove('main-search-input-button-shown');
+        searchCharacterRemoveButton.classList.remove('main-search-input-button-shown');
     };
 });
 
 // REMOVE ALL THE CHARACTERS
-searchButton.addEventListener('click', () => {
+searchCharacterRemoveButton.addEventListener('click', () => {
     searchInput.value = '';
-    searchButton.classList.remove('main-search-input-button-shown');
+    searchCharacterRemoveButton.classList.remove('main-search-input-button-shown');
 });
